54105b8f859702651553813f808db92409d6b168
Anna Karutina <anna.karutina@khk.ee>
2026-03-19 09:10:03 +0200

    Create base project

---
+++ src/main/java/com/mycompany/mywebapp/MywebappVso24Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.mywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebappVso24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MywebappVso24Application.class, args);
+    }
+
+}
